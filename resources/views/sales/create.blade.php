f1b3a92b91e45819019def2d8f2fb5e081cc6ba0
FilipeQuina <filipe.pacheco00@gmail.com>
2018-08-28 22:43:00 -0300

    Scroll bar para a tabela de vendas & add campo de valor total.

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -10,7 +10,7 @@
 
     <div class="row">
         <div class='col-md-4'>
-            <div class="form-control" style="margin-bottom: 10px">
+            <div class="form-control espacamento">
                 Pesquise o Produto:
                 <div class="input-group input-group-sm">
                     <input type="text" name="namePesquisa" id="namePesquisa">
@@ -24,6 +24,7 @@
                     {{ csrf_field() }}
                     <label for="orcamento">Orçamento?:</label>
                     <input type="hidden" name="orcamento" id="orcamento" value="1">
+                    <input type="hidden" name="valorTotal" id="valorTotal">
                     <input type="hidden" name="itensLista" id="itensLista">
 
                     <div>
@@ -48,35 +49,49 @@
             </div>
         </div>
         <div class="col-md-8">
-            <div class="form-control">
+            <div class="form-control ">
 
 
-                <div style="margin-bottom: 10px">
+                <div class="espacamento">
                     <button class="btn btn-warning" onclick="addlista()"> Adicionar Produtos </button>
                     <button class="btn btn-success float-right" id="fecharVenda"> Fechar venda </button>
                 </div>
+                <div class="limite-tabela">
 
-                <!--<div><a href="javascript:document.form.submit();">CADASTRAR</a>.</div>-->
-                <table class="table table-sm table-bordered" id="tabelaDeItens">
-                    <thead>
-                        <tr>
-                            <th>Código de barras</th>
-                            <th>Nome</th>
-                            <th>Preço</th>
-                            <th>Quantidade</th>
-                            <th>Valor Total</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody id="item"></tbody>
-                </table>
+                    <table class="table table-bordered table-condensed " id="tabelaDeItens">
+                        <thead>
+                            <tr>
+                                <th>Código de barras</th>
+                                <th>Nome</th>
+                                <th>Preço</th>
+                                <th>Quantidade</th>
+                                <th>Valor Total</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+
+                        <tbody id="item"></tbody>
+
+
+                    </table>
+                </div>
+
             </div>
+
+
+            <div class="col-md-2">
+                <label for="valorTotal">valor Total:</label>
+                <input type="text" id="valorTotal_input" class="form-control" readonly>
+            </div>
+
         </div>
+
     </div>
 </div>
-</div>
+
 <script>
     var listaDeItens = [];
+    var totalDaVenda = 0;
     function addlista() {
 
         var id = document.getElementById("id").value;
@@ -102,9 +117,12 @@
             tdPrice.innerHTML = price;
             tdQtd.innerHTML = qtd;
             tdVTotalItem.innerHTML = VTotalItem;
-            tdAcao.innerHTML = '<input type="button" class="btn btn-danger" value="Excluir" onclick="deleteRow(this.parentNode.parentNode.rowIndex)">';
+            tdAcao.innerHTML = '<input type="button" class="btn btn-danger btn-sm" value="Excluir" onclick="deleteRow(this.parentNode.parentNode.rowIndex)">';
 
             var itens = { id: id, qtd: qtd, price: price, VTotalItem: VTotalItem, };
+            var valorAtual = totalDaVenda += VTotalItem;
+                document.getElementById("valorTotal").value = valorAtual;
+            document.getElementById("valorTotal_input").value = valorAtual;
 
             tr.appendChild(tdId);
             tr.appendChild(tdName);
@@ -115,7 +133,8 @@
 
             document.getElementById("item").appendChild(tr);
             listaDeItens.push(itens);
-            
+
+
             document.getElementById("id").value = '';
             document.getElementById("name").value = '';
             document.getElementById("price").value = '';
@@ -124,8 +143,12 @@
     }
     function deleteRow(i) {
         document.getElementById('tabelaDeItens').deleteRow(i);
-        listaDeItens.splice(i-1,1);
-        console.log(i);
+        var valorAtual = totalDaVenda -= listaDeItens[i - 1]['price'];
+        document.getElementById("valorTotal").value = valorAtual;
+        document.getElementById("valorTotal_input").value = valorAtual;
+        listaDeItens.splice(i - 1, 1);
+
+        console.log(totalDaVenda);
         console.log(listaDeItens);
     }
     $(document).ready(function () {
